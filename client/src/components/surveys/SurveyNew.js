925517675c090b72eab71bb923642b3c68ee0ef9
Vannya <van.tabbert@gmail.com>
2018-01-27 22:28:35 -0600

    added auth check on surveynew page

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import { reduxForm } from "redux-form";
+import {Redirect} from 'react-router-dom';
 import SurveyForm from "./SurveyForm";
 import SurveyFormReview from "./SurveyFormReview";
 
@@ -28,11 +30,26 @@ class SurveyNew extends Component {
     );
   }
 
+  authCheck() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <Redirect to="/" />;
+      default:
+        return this.renderContent();
+    }
+  }
+
   render() {
-    return <div>{this.renderContent()}</div>;
+    return <div>{this.authCheck()}</div>;
   }
 }
 
-export default reduxForm({
-  form: "surveyForm"
-})(SurveyNew);
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(
+  reduxForm({ form: "surveyForm" })(SurveyNew)
+);
